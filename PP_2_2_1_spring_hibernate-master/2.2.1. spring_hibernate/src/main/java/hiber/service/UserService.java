9bf43ec13ac7f03d32145c1ce6d427c862fe10e9
Elina Korotkova <128074533+ElinaKorotkova@users.noreply.github.com>
2023-05-29 23:13:03 +0500

    First commit
    First commit

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,18 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+   // void add(User user, Car car);
+    void add(User user);
+    void add2(Car car);
+    List<User> listUsers();
+
+    List<Car> listCars();
+
+    User getUserCars(String model, int series);
+
+}
